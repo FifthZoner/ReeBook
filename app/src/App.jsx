c8e2c0a0fc9eb45e4885daa032f8dd3a16550879
karolmichonsky <karolmichonsky@gmail.com>
2024-04-15 23:10:03 +0200

    Feat: Add routing

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,21 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ButtonUsage from './ui/button'
-import Footer from './ui/footer'
-import Hero from './ui/hero'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Welcome from './pages/welcome'
 import SignInSide from './pages/signIn'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-      <h1>
-        <Hero />
-        <SignInSide />
-        <Footer />
-      </h1> 
+    <Router>
+    <Routes>
+      <Route path='/' element={<Welcome />} />
+      <Route path='/login' element={<SignInSide />} />
+    </Routes>
+  </Router>
   )
 }
 
