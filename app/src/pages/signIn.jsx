61a739e9ce293d035f8fcaf6fdbb33ab090673a4
karolmichonsky <karolmichonsky@gmail.com>
2024-04-20 23:59:12 +0200

    Feat: Add styling to signIn page

--- app/src/pages/signIn.jsx
+++ app/src/pages/signIn.jsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
-import LoginCard from "../cards/loginCard";
-import Book3dCard from "../cards/book3dCard";
+import hero from "../assets/library.jpg";
 
 function Copyright(props) {
   return (
@@ -29,7 +28,7 @@ export default function SignInSide() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch("http://localhost:5000/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -42,7 +41,7 @@ export default function SignInSide() {
       if (!response.ok) {
         throw new Error("Login failed");
       }
-      console.log("Logged in")
+      console.log("Logged in");
     } catch (err) {
       setError("Wrong pass or login");
     }
@@ -58,16 +57,22 @@ export default function SignInSide() {
   };
 
   return (
-    <div className="flex flex-wrap justify-center items-center min-h-screen bg-zinc-800">
-      <div className="bg-slate-700 text-white px-12 flex flex-wrap justify-center items-center rounded-3xl">
-        <div>
-          <Book3dCard />
+    <div className="bg-gradient-to-r from-orange-800 to-orange-700 min-h-screen flex items-center justify-center">
+      <div className="relative w-full max-w-3xl p-8 bg-purple-100 rounded-lg shadow-lg flex flex-row">
+        <div className="w-1/2">
+          <img src={hero} alt="" className="w-full rounded-lg shadow-lg" />
         </div>
-        <div>
-          <form onSubmit={handleLogin} className="flex flex-col justify-center items-center">
-            <label htmlFor="email" >Email:</label>
+
+        <div className="w-1/2">
+          <div className="mt-4 text-center"></div>
+          <h2 className="text-2xl font-semibold my-4 text-center">Sign In</h2>
+          <form
+            onSubmit={handleLogin}
+            className="flex flex-col justify-center items-center"
+          >
+            <label htmlFor="email">Email:</label>
             <input
-              className="text-black"
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-purple-500 my-2"
               type="text"
               id="email"
               value={email}
@@ -75,15 +80,24 @@ export default function SignInSide() {
             />
             <label htmlFor="password">Password:</label>
             <input
-              className="text-black"
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-purple-500 my-2 "
               type="password"
               id="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
-            <button type="submit">Login</button>
+            <button
+              type="submit"
+              className="w-full bg-orange-700 text-white py-2 rounded-md hover:bg-orange-800 my-2 transition duration-300"
+            >
+              Login
+            </button>
           </form>
-          {error && <p>{error}</p>}
+          {error && <p className=" text-center">{error}</p>}
+
+          <p className="mt-4 text-gray-600 text-center">
+            Not a member? Register now
+          </p>
         </div>
       </div>
     </div>
