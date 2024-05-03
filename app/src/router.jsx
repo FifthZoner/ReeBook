236b123354c5311bc10541673b0f799298db435f
karolmichonsky <karolmichonsky@gmail.com>
2024-05-03 23:48:13 +0200

    Feat: Create Protected Route
    For now without auth logic, waiting for back-end to prepare API

--- app/src/router.jsx
+++ app/src/router.jsx
@@ -1,11 +1,18 @@
-import { createBrowserRouter } from 'react-router-dom';
-import Login from './pages/login.jsx';
-import Welcome from './pages/welcome.jsx';
-import Main from './pages/main.jsx';
+import { createBrowserRouter } from "react-router-dom";
+import ProtectedRoute from './ProtectedRoute';
+import Login from "./pages/login.jsx";
+import Welcome from "./pages/welcome.jsx";
+import Main from "./pages/main.jsx";
 
 export const router = createBrowserRouter([
-    {path: "/", element: <Welcome/>},
-    {path: "/login", element: <Login/>},
-    {path: "/main", element: <Main/>},
+  { path: "/", element: <Welcome /> },
+  { path: "/login", element: <Login /> },
+  {
+    path: "/main",
+    element: (
+      <ProtectedRoute>
+        <Main />
+      </ProtectedRoute>
+    ),
+  },
 ]);
-  
