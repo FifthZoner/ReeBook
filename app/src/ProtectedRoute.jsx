c974340ce96dcb127148343965237fb38eb5cc11
karolmichonsky <karolmichonsky@gmail.com>
2024-05-13 22:29:13 +0200

    Feat: Connect protected router logic

--- app/src/ProtectedRoute.jsx
+++ app/src/ProtectedRoute.jsx
@@ -1,7 +1,43 @@
+import { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = true //Prepare some auth logic
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const isLogged = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://localhost:5000/api/checkSession", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: 'include'
+        });
+        if (response.status === 401) {
+          console.log("User session is not valid!");
+          setIsAuthenticated(false);
+        } else if (response.status === 200) {
+          console.log("Session is valid!");
+          setIsAuthenticated(true);
+        } else {
+          console.log("Error when checking session!");
+        }
+      } catch (err) {
+        setError("Error when checking session!");
+      }
+      setIsLoading(false);
+    };
+
+    isLogged();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   if (!isAuthenticated) {
     return <Navigate to="/login" />;
