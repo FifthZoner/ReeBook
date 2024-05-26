ff8ae875dc9abe2d1293475f4e58da4a35356b31
karolmichonsky <karolmichonsky@gmail.com>
2024-05-26 23:15:09 +0200

    Refactor: Delete redundant code

--- app/src/ui/hero.jsx
+++ app/src/ui/hero.jsx
@@ -25,44 +25,6 @@ const Hero = () => {
           EXPLORE
         </button>
       </div>
-      <div className="hidden lg:flex w-1/3 bg-black bg-opacity-20 rounded-3xl p-12  flex-col flex-1 gap-8 backdrop-blur-[3px] drop-shadow-xl max-h-full justify-center">
-        <div className=" bg-white rounded-2xl p-6 flex flex-row">
-          <img src={hero} alt="" className=" w-28 h-28 rounded-xl" />
-          <div className="flex flex-col flex-wrap mx-4 my-2">
-            <h1 className=" font-bold text-xl">Book name</h1>
-            <p className=" text-gray-400">Tags</p>
-            <div className=" flex flex-row gap-2 py-2">
-              <button className="bg-white drop-shadow-xl py-1 px-3 rounded-md ">
-                Info
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className=" bg-white rounded-2xl p-6 flex flex-row">
-          <img src={hero} alt="" className=" w-28 h-28 rounded-xl" />
-          <div className="flex flex-col flex-wrap mx-4 my-2">
-            <h1 className=" font-bold text-xl">Book name</h1>
-            <p className=" text-gray-400">Tags</p>
-            <div className=" flex flex-row gap-2 py-2">
-              <button className="bg-white drop-shadow-lg py-1 px-3 rounded-md">
-                Info
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className=" bg-white rounded-2xl p-6 flex flex-row">
-          <img src={hero} alt="" className=" w-28 h-28 rounded-xl" />
-          <div className="flex flex-col flex-wrap mx-4 my-2">
-            <h1 className=" font-bold text-xl">Book name</h1>
-            <p className=" text-gray-400">Tags</p>
-            <div className=" flex flex-row gap-2 py-2">
-              <button className="bg-white drop-shadow-lg py-1 px-3 rounded-md">
-                Info
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
